225b1fb3c91fb0e60790e87bc1b6f36b315f3614
Coldain <wolfpack.ethan@outlook.com>
2018-03-06 22:51:24 -0700

    finalize rebranding

--- RPGToolKit/Program.cs
+++ RPGToolKit/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace KnockoutDemo
+namespace RPGToollKit
 {
     public class Program
     {
